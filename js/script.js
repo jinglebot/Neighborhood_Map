3bd8c6c559f65248ddc6fe66876fb16e1a0e7919
jinglebot <jinglebot@users.noreply.github.com>
2016-06-30 07:33:38 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initMap () {
 
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 37.7749, lng: 122.4194},
+		center: {lat: 122.4194, lng: 37.7749},
 		scrollwheel: false,
 		zoom: 8
 	});
