3de009be1a5649fcff701d59e0e072be1eb33684
jinglebot <jinglebot@users.noreply.github.com>
2016-07-01 10:39:35 -0700

    Update script.js
    Change position values

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ function initMap () {
 
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 37.7749, lng: 122.4194},
+		center: {lat: -34.397, lng: 150.644},
 		scrollwheel: false,
 		zoom: 8
 	});
 	
 	var marker = new google.maps.Marker({
-		position = {lat: 37.4275, lng: 122.1697},
+		position = {lat: -34.397, lng: 150.644},
 		map = map;
 	});
 
