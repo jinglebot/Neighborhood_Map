09162bcaa12081801b71618930d5a3ad8e3ae215
jinglebot <jingle1216@yahoo.com>
2016-07-04 14:01:33 -0700

    Add infowindow

--- js/script.js
+++ js/script.js
@@ -3,9 +3,28 @@ function initMap () {
 
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
+		center: {lat: 37.4275, lng: 122.1697}, // Stanford University
 		scrollwheel: false,
-		zoom: 8
+		zoom: 12
 	});
+
+	var locations = [
+	{title: 'First', location: {lat: 37.4275, lng: 122.1697}}
+	// , {title: 'Second'},
+	// {title: 'Third'}
+	];
+
+	var marker = new google.maps.Marker({
+		position: locations,
+		map: map
+	});
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: "My first infoWindow"
+	});
+
+	marker.addListener('click', function(){
+		infoWindow.open(map, marker);
+	})
 
 };
