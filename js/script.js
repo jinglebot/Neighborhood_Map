365289b04d52d05db431df8f24b9e92e15aa720c
jinglebot <jingle1216@yahoo.com>
2016-07-05 12:34:53 -0700

    Add back latlng values

--- js/script.js
+++ js/script.js
@@ -4,23 +4,24 @@ function initMap () {
 
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {street: '427 N. Camden Drive, Suite F', city: 'Beverly Hills, CA 90210'},
+		center: {lat: -118.40453, lng: 34.069057},
 		scrollwheel: false,
 		zoom: 12
 	});
 
 	var locations = [
-		{title: 'Los Angeles Gold & Silver', location: {street: '427 N. Camden Drive, Suite F', city: 'Beverly Hills, CA 90210'}},
+		{title: 'Los Angeles Gold & Silver', location: {street: '427 N. Camden Drive, Suite F', city: 'Beverly Hills, CA 90210'}, latlng: {lat: -118.40453, lng: 34.069057}},
 		{title: 'Huntington Rare Coins & Precious Metals', location: {street: '31 W Del Mar Blvd.', city: 'Pasadena, CA 91105'}},
 		{title: '310antiques', location: {street: '3159 Donald Douglas Loop South #305', city: 'Santa Monica, CA 90405'}}
 	];
 
 	var largeInfowindow = new google.maps.InfoWindow();
-	var bounds = new google.maps.LatLngBoundsLiteral();
+	var bounds = new google.maps.LatLngBounds();
 
 	for (var i = 0; i < locations.length; i++) {
 		var position = locations[i].location.street + ' ' + locations[i].location.city;
 		var title = locations[i].title;
+		var latlng = locations[i].latlng;
 		var marker = new google.maps.Marker({
 			position: position,
 			map: map,
@@ -30,7 +31,7 @@ function initMap () {
 		});
 
 		markers.push(marker);
-		bounds.extend(marker.position);
+		bounds.extend(marker.latlng);
 
 		// var infoWindow = new google.maps.InfoWindow({
 		// 	content: title
