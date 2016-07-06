4e8b8a6f3432c01ec848a4c6d3b45a32ed751a2f
jinglebot <jinglebot@users.noreply.github.com>
2016-07-05 17:00:51 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function initMap () {
 		});
 
 		markers.push(marker);
-		bounds.extend(marker.latlng);
+		bounds.extend(marker.latlng.lat, marker.latlng.lng);
 
 		// var infoWindow = new google.maps.InfoWindow({
 		// 	content: title
