c51e1c22cc946a92cca34172bd068356af866ed5
jinglebot <jingle1216@yahoo.com>
2016-09-03 09:23:52 -0700

    Add list functionality

--- README.md
+++ README.md
@@ -1,30 +1,45 @@
 # FRONTEND NANODEGREE NEIGHBORHOOD MAP PROJECT
 
-To do:
 
-A.
+
+A. To do:
 
 1. do layout of location list and display
 
-2. do list, do display
+	- Bootstrap makes it easy
 
-3. update to knockout
+2. create list <ul>, update to knockout
 
-4. add map
+	- <ul> by knockout, map by js/jQuery
 
-B.
-1. get Google map to display
-  -	get API key
-  -	use lat/lng values to display the map
-  -	switch from lat/lng to street/city values to indicate locations
-	  and see which works with API methods
-2. complete an array of locations to use
-	-	get at least 5 locations with title, address and lat/lng values
-3. put markers for the list of locations on the map
-	-	[ ] get markers to show using street/city names(didn't work)
-	-	[ ] get markers to show using lat/lng values (included values in the array)
-	- [X]	get geocode instead of hardcoded lat/lng values
-4. implement a list view of locations on the page
-	-	[ ]	download knockout framework
-	-	[X]	separate concerns in mvc style: model, view and viewmodel
-	-	[X]	separate maps and markers, etc
+3. create map display, add markers
+
+4. get <li>s linked to markers and infowindow on click
+
+	- <li> by knockout, markers by js/jQuery
+
+
+B. Problems encountered:
+
+1.  [x] get Google map to display
+
+	- get API key
+
+2.  [x] complete an array of locations to use
+
+	- get at least 5 locations with title, address and lat/lng values
+
+3.  [x] put markers on the map
+
+	- [x] get markers to show using lat/lng values in the array
+
+4.  [ ] get infowindow to show on click of markers, on click of <li>s
+
+5.  [ ] add filter functionality
+
+6.  [ ] make sure there is separation of concerns in mvc style: model, view and viewmodel
+
+7.  [ ] how to solve error "google is undefined"
+
+
+
--- js/script.js
+++ js/script.js
@@ -5,75 +5,75 @@
 			title: '310antiques',
 			street: '3159 Donald Douglas Loop South #305',
 			city: 'Santa Monica, CA 90405',
-			loc: ['lat: 34.017094', 'lng: -118.446815']
+			loc: {lat: 34.017094, lng: -118.446815}
 		},
 		{
 			title: 'A Coin Exchange',
 			street: '18631 Ventura Blvd.',
 			city: 'Tarzana, CA 91356',
-			loc: ['lat: 34.170143', 'lng: -118.53939']
+			loc: {lat: 34.170143, lng: -118.53939}
 		},
-	// 	{
-	// 		title: 'Collectors Internet',
-	// 		street: '7510 Sunset Blvd. #183',
-	// 		city: 'Los Angeles, CA 90046',
-	// 		location: {lat: 34.09802, lng:  -118.35311}
-	// 	},
-	// 	{
-	// 		title: 'Los Angeles Gold & Silver',
-	// 		street: '427 N. Camden Drive, Suite F',
-	// 		city: 'Beverly Hills, CA 90210',
-	// 		location: {lat: 34.069057, lng: -118.40453}
-	// 	},
-	// 	{
-	// 		title: 'Jewelry 1on1',
-	// 		street: '5351 Topanga Canyon Blvd.',
-	// 		city: 'Woodland Hills CA 91364',
-	// 		location: {lat: 34.167366, lng: -118.60577}
-	// 	},
-	// 	{
-	// 		title: 'Meridian Coin',
-	// 		street: '22330 Hawthorne Blvd Suite C',
-	// 		city: 'Torrance, CA 90505',
-	// 		location: {lat: 33.82483, lng: -118.35113}
-	// 	},
-	// 	{
-	// 		title: 'Pacific Coast Coin & Currency',
-	// 		street: '11696 Ventura Blvd.',
-	// 		city: 'Studio City, CA 91604',
-	// 		location: {lat: 34.14116, lng: -118.38743}
-	// 	},
-	// 	{
-	// 		title: 'Paul Albarian & Associates',
-	// 		street: '3500 West Olive Ave. 3F, Suite 300',
-	// 		city: 'Burbank, CA 91505',
-	// 		location: {lat: 34.15284, lng: -118.33816}
-	// 	},
-	// 	{
-	// 		title: 'South Bay Gold + Finley’s Jewelers',
-	// 		street: '3804 Sepulveda Blvd Suite C',
-	// 		city: 'Torrance, Ca 90505',
-	// 		location: {lat: 33.825577, lng: -118.35147}
-	// 	},
-	// 	{
-	// 		title: 'Southern California Coins & Stamps',
-	// 		street: '7635 Firestone Blvd.',
-	// 		city: 'Downey, CA 90241',
-	// 		location: {lat: 33.946266, lng:-118.14451}
-	// 	},
-	// 	{
-	// 		title: 'Wilshire Coin',
-	// 		street: '1312 Lincoln Blvd.',
-	// 		city: 'Santa Monica CA 90401',
-	// 		location: {lat: 34.020596, lng: -118.49259}
-	// 	},
+		{
+			title: 'Collectors Internet',
+			street: '7510 Sunset Blvd. #183',
+			city: 'Los Angeles, CA 90046',
+			loc: {lat: 34.09802, lng:  -118.35311}
+		},
+		{
+			title: 'Los Angeles Gold & Silver',
+			street: '427 N. Camden Drive, Suite F',
+			city: 'Beverly Hills, CA 90210',
+			loc: {lat: 34.069057, lng: -118.40453}
+		},
+		{
+			title: 'Jewelry 1on1',
+			street: '5351 Topanga Canyon Blvd.',
+			city: 'Woodland Hills CA 91364',
+			loc: {lat: 34.167366, lng: -118.60577}
+		},
+		{
+			title: 'Meridian Coin',
+			street: '22330 Hawthorne Blvd Suite C',
+			city: 'Torrance, CA 90505',
+			loc: {lat: 33.82483, lng: -118.35113}
+		},
+		{
+			title: 'Pacific Coast Coin & Currency',
+			street: '11696 Ventura Blvd.',
+			city: 'Studio City, CA 91604',
+			loc: {lat: 34.14116, lng: -118.38743}
+		},
+		{
+			title: 'Paul Albarian & Associates',
+			street: '3500 West Olive Ave. 3F, Suite 300',
+			city: 'Burbank, CA 91505',
+			loc: {lat: 34.15284, lng: -118.33816}
+		},
+		{
+			title: 'South Bay Gold + Finley’s Jewelers',
+			street: '3804 Sepulveda Blvd Suite C',
+			city: 'Torrance, Ca 90505',
+			loc: {lat: 33.825577, lng: -118.35147}
+		},
+		{
+			title: 'Southern California Coins & Stamps',
+			street: '7635 Firestone Blvd.',
+			city: 'Downey, CA 90241',
+			loc: {lat: 33.946266, lng:-118.14451}
+		},
+		{
+			title: 'Wilshire Coin',
+			street: '1312 Lincoln Blvd.',
+			city: 'Santa Monica CA 90401',
+			loc: {lat: 34.020596, lng: -118.49259}
+		},
 		{
 			title: 'Huntington Rare Coins & Precious Metals',
 			street: '31 W Del Mar Blvd.',
 			city: 'Pasadena, CA 91105',
-			loc: ['lat: 34.140766', 'lng: -118.15207']
+			loc: {lat: 34.134558, lng: -118.322478}
 		}
-
+	];
 
 	var Locate = function(data) {
 			this.title = data.title;
@@ -83,193 +83,115 @@
 	};
 
 	// VIEW
-	var View = function () {
-		// initList: function() {
-	// var map;
-	// var markers = [];
-	// function initMap () {
+	var map;
+	var markers = [];
 
-	// Create a map object and specify the DOM element for display.
-	// var map = new google.maps.Map(document.getElementById('map'), {
-	// 	center: {lat: -118.40453, lng: 34.069057},
-	// 	scrollwheel: false,
-	// 	zoom: 13
-	// });
-
-	// function initMap () {
-
-		// Create a map object and specify the DOM element for display.
-		var map;
-		var markers = [];
-		var geocoder;
-
+	function initMap() {
+		// make map
 		var map = new google.maps.Map(document.getElementById('map'), {
-			center: {lat: -118.40453, lng: 34.069057},
+			center: {lat: 34.134558, lng:-118.322478},
 			scrollwheel: false,
 			zoom: 13
 		});
-	// }
 
+		// make markers
+		var largeInfowindow = new google.maps.InfoWindow();
+		var bounds = new google.maps.LatLngBounds();
 
-	// var largeInfowindow = new google.maps.InfoWindow();
-	// var bounds = new google.maps.LatLngBounds();
-	// function createMapMarker(placeData) {
-	// 	var lat = placeData.geometry.location.lat();  // latitude from the place service
-	// 	var lon = placeData.geometry.location.lng();  // longitude from the place service
-	// 	var latlng = 'lat: ' + lat + ', ' + 'lng: ' + lon;
-		// var name = placeData.formatted_address;   // name of the place from the place service
-		// var position = placeData.geometry.location;
-		// return latlng;
-		// var bounds = window.mapBounds;            // current boundaries of the map window
-		// var marker = new google.maps.Marker({
-		// 	map: map,
-		// 	position: placeData.geometry.location,
-		// 	title: name
-		// });
+		for (var i = 0; i < initLocations.length; i++) { 	// for every object in array locations
+			var title = initLocations[i].title;
+			var position = initLocations[i].loc;
+			// console.log(position);
+			var marker = new google.maps.Marker({
+				map: map,
+				position: position,
+				title: title,
+				animation: google.maps.Animation.DROP,
+				id: title
+			});
 
-		// var contentString = '<div id="content" class="center-content">'+'<h3><b>'+name+'</b></h3>'+'</div>';
+			markers.push(marker);
+			bounds.extend(marker.position);
+			marker.addListener('click', function() {
+				populateInfowindow(this, largeInfowindow);
+			});
+		}
 
-		// var infoWindow = new google.maps.InfoWindow({
-		// 	content: contentString
-		// });
+		// for checking
+		// for (var i = 0; i < markers.length; i++){
+		// 	console.log(markers[i].title);
+		// }
 
-		// google.maps.event.addListener(marker, 'click', function() {
-			// infoWindow.open(map, marker);
-		// });
-
-	    // bounds.extend() takes in a map location object
-		// bounds.extend(new google.maps.LatLng(lat, lon));
-    	// fit the map to the new marker
-		// map.fitBounds(bounds);
-    	// center the map
-		// map.setCenter(bounds.getCenter());
+		map.fitBounds(bounds);
+		map.setCenter(bounds.getCenter());
 	}
 
-	// for (var i = 0; i < locations.length; i++) {
-	// 	// var position = locations[i].latlng;
-	// 	var title = locations[i].title;
-	// 	var loc = locations[i].location.street + ', ' + locations[i].location.city;
-		// var loc = locations[i].location;
-		// var lat = loc.geometry.location.lat();
-		// var lng = loc.geometry.location.lng();
-		// var position = createMapMarker(loc);
-		// var marker = new google.maps.Marker({
-		// 	position: position,
-		// 	map: map,
-		// 	title: title,
-		// 	animation: google.maps.Animation.DROP,
-		// 	id: i
-		// });
+	// for each marker's infowindow
+	function populateInfowindow(marker, infowindow) {
+		if (infowindow.marker != marker) {
+			infowindow.marker = marker;
+			infowindow.setContent('<div id="content" class="center-content">'+'<h3><b>'+ marker.title +'</b></h3>'+'</div>');
+			infowindow.open(map, marker);
+			// infowindow.addListener('closeclick', function(){
+			// 	infowindow.setMarker(null);
+			// });
+		}
+	}
 
-
-
-	};
 
 	var ViewModel = function() {
-
-		// initialize View
-		View();
-
 		// self === ViewModel
 		var self = this;
 
-		// viewmodel's array declaration
+		// viewmodel's array
 		this.locationList = ko.observableArray([]);
 
-		// initializes and stores locationList with items from initLocations
+		// initializes locationList array with items from initLocations array
 		initLocations.forEach(function(locItem){
 			var loc = new Locate(locItem);
 			self.locationList.push( loc );
 		});
 
-	// Search bar
-
-		// save input value
+		// viewmodel's filter storage
 		this.searchBar = ko.observable("");
-		this.searchArray = ko.observableArray([]);
 
-// TODO: how to filter array
-		// Filters out locations in input value
-		// on change of searchBar input
-		this.setFilter = function() {
-			console.log("hi");
-		};
+		// initializes locationList array items as visible
+		this.locationList().forEach(function(locItem){
+			locItem.showItem = ko.observable(true);
+			 //console.log(locItem.showItem());
+		});
+
+		// viewmodel's filtered list storage
+		this.filteredLocationList = ko.computed(function() {
+			// if searchBar is empty
+			if (!self.searchBar() ) {
+				return self.locationList();
+			} else {
+				// get the array elements
+				return ko.utils.arrayFilter(self.locationList(), function(prod) {
+					// get the array elements title
+					var title = prod.title;
+					// compare title to searchBar
+					// for (var i = 0; i < title.length; i++ ) {
+					if (title != self.searchBar()) {
+						prod.showItem(false);
+						console.log(title + " " + prod.showItem());
+						return prod.showItem();
+					}
+				});
+			}
+		});
 
 // TODO:
-		// save input value to searchBar
-		// connect searchBar to locationList
-		// filter locationList
-			// find similar data from locations array
-			// for (var i = 0; i < locationList.length; i++) { 	// for every object in array locations
-			//		var loc = locationList[i];
-			// 		foreach (var n in loc) {			// for every property in each object
-			//			var prop = loc[n];
-			// 			foreach (var letter in prop.title) {	// for each letter in the property value
-			// 				var excerpt = splice(letter, prop.title);	// splice thru the property value to compare if equal to this.name
-			// 				if (searchBar == excerpt) {
-			//					put display = visible
-			// }	}	}	}
-			// erase all <li>
-			// display new <ul> with property = visible
-			// if searchBar = " " () {
-			// 		display all or refresh }
-			//  put markers of new <li> on map == markers of filtered locationList
+		// filter list
+		// connect searchBar to locationList to filter list
+		// put markers of  <ul> on map == markers of filtered locationList
+		// connect listener for clicking list to show marker and infowindow
+
+// Qs:
+		// do you filter locationList or do you make a new array? standard or knockout?
+		// how do you get google to work inside Viewmodel so I can put markers on locationList?
 
 	};
 
 	ko.applyBindings(new ViewModel() );
-
-// 	// VIEW INITIAL DISPLAY
-// 	geocoder = new google.maps.Geocoder();
-// 	var largeInfowindow = new google.maps.InfoWindow();
-// 	var bounds = new google.maps.LatLngBounds();
-
-// 	function codeAddress(address) {
-// 		geocoder.geocode({'address': address}, function(results, status){
-// 			if (status == google.maps.GeocoderStatus.OK){
-// 				address.position = address.geometry.location;
-// 			}
-// 		});
-// 	}
-
-
-
-// 	// VIEW DISPLAY
-// 	function createMapMarker(placeData) {
-// 		var lat = placeData.geometry.location.lat();  // latitude from the place service
-// 		var lon = placeData.geometry.location.lng();  // longitude from the place service
-// 		var latlng = 'lat: ' + lat + ', ' + 'lng: ' + lon;
-// 		// var name = placeData.formatted_address;   // name of the place from the place service
-// 		var position = placeData.geometry.location;
-// 		return latlng;
-// 		// var bounds = window.mapBounds;            // current boundaries of the map window
-// 		// var marker = new google.maps.Marker({
-// 		// 	map: map,
-// 		// 	position: placeData.geometry.location,
-// 		// 	 title: name
-// 		// });
-
-//     // infoWindows are the little helper windows that open when you click
-//     // or hover over a pin on a map. They usually contain more information
-//     // about a location.
-// 		// var contentString = '<div id="content" class="center-content">'+'<h3><b>'+name+'</b></h3>'+'</div>';
-
-// 		// var infoWindow = new google.maps.InfoWindow({
-// 		// 	content: contentString
-// 		// });
-
-//     // hmmmm, I wonder what this is about...
-// 		// google.maps.event.addListener(marker, 'click', function() {
-// 	// your code goes here!
-// 			// infoWindow.open(map, marker);
-// 		// });
-
-//     // this is where the pin actually gets added to the map.
-//     // bounds.extend() takes in a map location object
-// 		// bounds.extend(new google.maps.LatLng(lat, lon));
-//     // fit the map to the new marker
-// 		// map.fitBounds(bounds);
-//     // center the map
-// 		// map.setCenter(bounds.getCenter());
-// 	}
-
